787c8dfe45382b42feedbfa4e07e05d6a55f1bf3
RedsAnDev <code@redsandev.com>
2016-02-29 00:23:57 +0100

    Ready, need to set css and some other stuff

--- mod_videobackground.php
+++ mod_videobackground.php
@@ -1,12 +1,8 @@
 
 <?php defined('_JEXEC') or die;
- 
+
 $moduleclass_sfx = htmlspecialchars($params->get("moduleclass_sfx"));
-$sourcevideo = htmlspecialchars($params->get("sourcevideo"));
-$autoplay = htmlspecialchars($params->get("autoplay"));
-$image = htmlspecialchars($params->get("image"));
 // Interroga G+ e valorizza la variabile $posts per una successiva visualizzazione
- 
+
 // Richiama il layout selezionato per questo modulo
 require JModuleHelper::getLayoutPath("mod_videobackground", $params->get('layout', 'default'));
- 

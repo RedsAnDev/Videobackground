5cc0bb0cf7bbf71e4c3b6fbbd508dc0ee59fe2ad
Andrea Rossi <code@redsandev.com>
2016-02-29 14:56:03 +0100

    Ready to compatible with youtube and local video

--- mod_videobackground.php
+++ mod_videobackground.php
@@ -3,6 +3,68 @@
 
 $moduleclass_sfx = htmlspecialchars($params->get("moduleclass_sfx"));
 // Interroga G+ e valorizza la variabile $posts per una successiva visualizzazione
+?>
+<div class="video-background <?php echo $moduleclass_sfx; ?>" data-target="video.video">
+  <?php if($params->get("stream")): ?>
+    <video class="video"
+    src="<?php echo htmlspecialchars($params->get("video"));?>" poster="<?php echo htmlspecialchars($params->get("image"));?>"
+    <?php echo ($params->get("autoplay")) ? "autoplay" : "";?>
+    <?php echo ($params->get("muted")) ? "muted" : "";?>
+    <?php echo ($params->get("loop")) ? "loop" : "";?>
+     >
+    </video>
+    <div class="controller">
+      <a href="#" class="cta-button" data-target="video.video">Test</a>
+    </div>
+    <script type="javascript">
+    jQuery(document).ready(function($) {
+        $('.cta-button').add('video.video').click(function() {
+          var video=$(this).data('target');
+            if(video.get(0).paused){
+              video.get(0).play()
+              $('div.message').fadeOut('slow');
+            }
+            else{
+             video.get(0).pause();
+             $('div.message').fadeIn('slow');
+           }
 
+        });
+    });
+    </script>
+<?php else:?>
+  <iframe id="ytplayer" class="video" type="text/html" width="100%" height="100vh"
+  src="https://www.youtube.com/embed/<?php echo $params->get('youtube');?>?&controls=0&showinfo=0&rel=1&playlist=<?php
+    echo $params->get('youtube');
+    echo (!$params->get("autoplay")) ? "&autoplay=0" : "&autoplay=1";
+    echo ($params->get("loop")) ? "&loop=1" : "";?>&origin=<?php echo $baseurl;?>"
+  frameborder="0"></iframe>
+<?php endif;?>
+<?php
 // Richiama il layout selezionato per questo modulo
 require JModuleHelper::getLayoutPath("mod_videobackground", $params->get('layout', 'default'));
+?>
+</div>
+<style>
+  .video-background{
+    width:100%;
+    position: relative;
+    min-height: 400px;
+  }
+  .video,iframe.video, iframe.video iframe{
+    width: 100%;
+    height: 100vh;
+    background-size: cover;
+    z-index: 0;
+  }
+  .message{
+    position: absolute;
+    top:20%;
+    padding:0px;
+    margin:0px;
+    width:100%;
+    background-color: rgba(255,255,255,0.7);
+    z-index:1;
+  }
+</style>
+<?php
--- tmpl/default.php
+++ tmpl/default.php
@@ -1,26 +1,4 @@
 <?php defined('_JEXEC') or die; ?>
-<style>
-  .video-background{
-    width:100%;
-    position: relative;
-    min-height: 400px;
-  }
-  .video{
-    width: 100%;
-    height: 100%;
-    background-size: cover;
-  }
-  .message{
-    position: absolute;
-    top:20%;
-    padding:5px 30px;
-    width:100%;
-    background-color: rgba(255,255,255,0.7);
-  }
-</style>
-<div class="video-background <?php echo $moduleclass_sfx; ?>" data-target="video.video">
-  <video muted class="video" src="<?php echo htmlspecialchars($params->get("video"));?>" poster="<?php echo htmlspecialchars($params->get("image"));?>">
-  </video>
   <div class="message">
     <?php if($params->get("show_maintitle")):?>
       <p class="h1"><?php echo htmlspecialchars($params->get("maintitle"));?></p>
@@ -34,25 +12,3 @@
 
         <?php endif;?>
   </div>
-  <div class="controller">
-    <a href="#" class="cta-button" data-target="video.video">Test</a>
-  </div>
-</div>
-
-
-<script type="javascript">
-jQuery(document).ready(function($) {
-    $('.cta-button').add('video.video').click(function() {
-      var video=$(this).data('target');
-        if(video.get(0).paused){
-          video.get(0).play()
-          $('div.message').fadeOut('slow');
-        }
-        else{
-         video.get(0).pause();
-         $('div.message').fadeIn('slow');
-       }
-
-    });
-});
-</script>
